c3ad6698cc4577abeca7561bddf8277c86ea9a21
Youssef <47580392+YJouhar47@users.noreply.github.com>
2025-05-25 19:05:31 +0200

    Fixed padding picture

--- src/components/ExperienceAndSkills.vue
+++ src/components/ExperienceAndSkills.vue
@@ -28,8 +28,8 @@
                 <div class="space-y-8 py-8" data-aos="fade-left">
                     <div v-for="element in Experiences" :key="element.id"
                     class="flex items-center rounded-xl p-4 bg-[#111a3e] shadow-lg border-[#1f1641]">
-                    <div class="w-1/4">
-                        <img src="https://img.icons8.com/ios-filled/100/ffffff/lawyer.png" alt="lawyer picture" class="pr-3">
+                    <div class="pr-3">
+                        <img src="https://img.icons8.com/ios-filled/100/ffffff/lawyer.png" alt="lawyer picture">
                     </div>
                     <div class="w-3/4 p1-4">
                         <h3 class="text-2xl font-semibold uppercase text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary">
