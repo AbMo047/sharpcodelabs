99c55c9987d609b4b8752970e7e4d09382aa6fb5
Youssef <47580392+YJouhar47@users.noreply.github.com>
2025-06-13 01:07:00 +0200

    Navbar color fixed

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,10 +6,10 @@
     <div class="md:hidden z-30">
       <button type="button" class="block focus:outline-none" @click="isMenuOpen = !isMenuOpen">
         <span v-if="isMenuOpen" class="text-5xl">
-          <img src="https://img.icons8.com/ios-filled/100/ffffff/delete-sign.png" alt="close" width="50" height="50"/>
+          <img src="https://img.icons8.com/ios-filled/100/dbb960/delete-sign.png" alt="close" width="50" height="50"/>
         </span>
         <span v-else class="text-5xl">
-          <img src="https://img.icons8.com/ios-filled/100/ffffff/menu--v6.png" alt="menu" width="50" height="50"/>
+          <img src="https://img.icons8.com/ios-filled/100/dbb960/menu--v6.png" alt="menu" width="50" height="50"/>
         </span>
       </button>
     </div>
